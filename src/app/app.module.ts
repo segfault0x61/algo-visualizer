75a274b48a044826b9a06e89c7d15475ac085d3c
segfault0x61 <torresj1997@outlook.com>
2022-05-06 12:34:06 -0500

    initial setup with angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlgorithmVisualizerComponent } from './algorithm-visualizer/algorithm-visualizer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlgorithmVisualizerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
